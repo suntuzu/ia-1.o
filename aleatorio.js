350699886ffc2d0ee4de00c4bce67a24f9c9fc91
suntuzu <165701007+suntuzu@users.noreply.github.com>
2024-08-06 11:13:21 -0300

    .

---
+++ aleatorio.js
@@ -0,0 +1,9 @@
+
+const nomes = ["Julia", "Dayana", "Maria Cecilia", "Mathew", "Ana Clara", "Guilhermo", "Gabriela"];
+
+export function aleatorio(lista) {
+    const posicao = Math.floor(Math.random() * lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
